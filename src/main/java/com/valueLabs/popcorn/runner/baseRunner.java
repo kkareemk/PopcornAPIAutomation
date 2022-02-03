b1d4ebadcde5901c4d441ad0c597a7a95cbee08c
kkareemk <kareemk.it@gmail.com>
2022-02-04 02:52:55 +0530

    Framework to validate REST API's

---
+++ src/main/java/com/valueLabs/popcorn/runner/baseRunner.java
@@ -0,0 +1,17 @@
+package com.valueLabs.popcorn.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.valueLabs.popcorn.stepDef",
+        monochrome = true,
+        plugin = {"pretty","html:target/cucumber","json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+
+ public class baseRunner {
+}
